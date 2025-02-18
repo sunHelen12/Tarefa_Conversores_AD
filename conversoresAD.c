ed47d032479f5c8c3a39c5ac532b38f62a98db18
sunHelen12 <sunrise.developer12@gmail.com>
2025-02-18 17:36:45 -0300

    Implementação do PWM com Joystick

--- conversoresAD.c
+++ conversoresAD.c
@@ -5,113 +5,89 @@
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 #include "pico/time.h"
-
-// Configurando fontes
 #include "inc/ssd1306.h"
 #include "inc/font.h"
 
-// Configurando o display
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define endereco 0x3C
 
-// Configurando o joystick
-#define JOYSTICK_X_PIN 26 // GPIO para eixo x
-#define JOYSTICK_Y_PIN 27 // GPIO para eixo y
-#define JOYSTICK_BUTTON_PIN 22 // GPIO para botão do Joystick
+#define JOYSTICK_X_PIN 26
+#define JOYSTICK_Y_PIN 27
+#define JOYSTICK_BUTTON_PIN 22
+#define BUTTON_A_PIN 5
 
-// Configurando do botão
-#define BUTTON_A_PIN 5 // GPIO para botão
+#define LED_VERDE_PIN 11
+#define LED_AZUL_PIN 12
+#define LED_VERMELHO_PIN 13
 
-// Configurando LEDs
-#define LED_VERDE_PIN 11 // GPIO para LED verde
-#define LED_AZUL_PIN 12 // GPIO para LED azul
-#define LED_VERMELHO_PIN 13 // GPIO para LED vermelho
-
-// Valores iniciais e constantes
 #define ADC_MAX_VALUE 4095
 #define JOYSTICK_CENTER_VALUE 2048
 #define BLACK 0
 #define WHITE 1
 
-// Variáveis globais
-static volatile uint32_t last_time = 0; // Armazena o último evento de tempo (microssegundos)
-static volatile bool leds_ativos = true; // Armazena o estado dos LEDs
-static volatile bool borda_display = false; // Alterna a borda do display
-ssd1306_t ssd; // Para armazenar o estado do display
+static volatile uint32_t last_time = 0;
+static volatile bool leds_ativos = true;
+static volatile bool borda_display = false;
+ssd1306_t ssd;
 
-// Prototipos das rotinas
 void init_hardware(void);
 void update_display(int x, int y);
 int map(int x, int in_min, int in_max, int out_min, int out_max);
 void gpio_irq_handler(uint gpio, uint32_t events);
-uint pwm_init_gpio(uint gpio, uint wrap);
+void set_led_brightness(uint gpio, uint16_t value);
+void init_pwm(uint gpio);
+uint16_t leitura_joystick(uint canal);
+void atualizar_pwm_leds(void);
 
-// Rotina principal
 int main() {
     init_hardware();    
-
     while (true) {
-        // Leitura do ADC do eixo X e Y do joystick
-        adc_select_input(0); // Escolhe o canal do eixo x
-        uint16_t adc_x = adc_read(); // Leitura do eixo x
-        adc_select_input(1); // Escolhe o canal do eixo y
-        uint16_t adc_y = adc_read(); // Leitura do eixo y
+        adc_select_input(0);
+        uint16_t adc_x = adc_read();
+        adc_select_input(1);
+        uint16_t adc_y = adc_read();
 
-        // Mapeamos os valores do joystick para o display
         int pos_y = map(adc_y, 0, ADC_MAX_VALUE, 0, WIDTH - 8);
         int pos_x = map(adc_x, 0, ADC_MAX_VALUE, HEIGHT - 8, 0);
+        update_display(pos_y, pos_x);
 
-        update_display(pos_y, pos_x);           
+        atualizar_pwm_leds();
 
-        sleep_ms(100); // Aguarda 100ms para atualizar o display
-       
+        sleep_ms(100);
     }
     return 0;
 }
 
-// Inicializa o hardware
 void init_hardware() {
     stdio_init_all();
-
-    // Inicializa o ADC e o joystick
     adc_init();
     adc_gpio_init(JOYSTICK_X_PIN);
     adc_gpio_init(JOYSTICK_Y_PIN);
     gpio_init(JOYSTICK_BUTTON_PIN);
     gpio_set_dir(JOYSTICK_BUTTON_PIN, GPIO_IN);
     gpio_pull_up(JOYSTICK_BUTTON_PIN);
-    // Configura a interrupção do botão do joystick
     gpio_set_irq_enabled_with_callback(JOYSTICK_BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-
-    // Inicializa o botão A
+    
     gpio_init(BUTTON_A_PIN);
     gpio_set_dir(BUTTON_A_PIN, GPIO_IN);
     gpio_pull_up(BUTTON_A_PIN);
-    // Configura a interrupção do botão A
     gpio_set_irq_enabled_with_callback(BUTTON_A_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
-    // Inicializa os LEDs
     gpio_init(LED_VERDE_PIN);
     gpio_set_dir(LED_VERDE_PIN, GPIO_OUT);
-    gpio_put(LED_VERDE_PIN, 0); // Desliga o LED verde
+    gpio_put(LED_VERDE_PIN, 0);
 
-    gpio_init(LED_AZUL_PIN);
-    gpio_set_dir(LED_AZUL_PIN, GPIO_OUT);
-    gpio_put(LED_AZUL_PIN, 0); // Desliga o LED azul
+    init_pwm(LED_AZUL_PIN);
+    init_pwm(LED_VERMELHO_PIN);
 
-    gpio_init(LED_VERMELHO_PIN);
-    gpio_set_dir(LED_VERMELHO_PIN, GPIO_OUT);
-    gpio_put(LED_VERMELHO_PIN, 0); // Desliga o LED vermelho
-
-    // Configura o I2C para o SSD1306
     i2c_init(I2C_PORT, 400 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // Inicializa o display OLED
+    
     ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
     ssd1306_config(&ssd);
     ssd1306_send_data(&ssd);
@@ -119,51 +95,63 @@ void init_hardware() {
     ssd1306_send_data(&ssd);
 }
 
-// Atualiza o display com a posição do joystick
-void update_display(int y, int x) {
-    ssd1306_fill(&ssd, BLACK); // Preenche o display com a cor preta
+void atualizar_pwm_leds(void) {
     
-    // Pressiona o botão do joystick
-    if (borda_display) {
-        ssd1306_fill(&ssd, !WHITE); // Preenche o display com a cor inversa
-        ssd1306_rect(&ssd, 3, 3, 122, 60, WHITE, BLACK); // Desenha a borda do display
-    }
+    uint16_t y_valor = leitura_joystick(0);
+    uint16_t x_valor = leitura_joystick(1);
 
-    
-    ssd1306_rect(&ssd, x, y, 8, 8, WHITE, WHITE); // Desenha o quadrado
-    ssd1306_send_data(&ssd); // Envia os dados para o display
+    uint8_t iluminacao_x = abs(x_valor - 1893) * 16 / 1893;
+    uint8_t iluminacao_y = abs(y_valor - 2099) * 16 / 2099;
+
+    uint16_t brilho_vermelho = iluminacao_x * 4095;
+    uint16_t brilho_azul = iluminacao_y * 4095;
+
+    set_led_brightness(LED_VERMELHO_PIN, brilho_vermelho);
+    set_led_brightness(LED_AZUL_PIN, brilho_azul);
 }
 
-// Função para mapear valores
+uint16_t leitura_joystick(uint canal) {
+    adc_select_input(canal);
+    return adc_read();
+}
+
+void update_display(int y, int x) {
+    ssd1306_fill(&ssd, BLACK);
+    if (borda_display) {
+        ssd1306_fill(&ssd, !WHITE);
+        ssd1306_rect(&ssd, 3, 3, 122, 60, WHITE, BLACK);
+    }
+    ssd1306_rect(&ssd, x, y, 8, 8, WHITE, WHITE);
+    ssd1306_send_data(&ssd);
+}
+
 int map(int x, int in_min, int in_max, int out_min, int out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-
-
-// Rotina de interrupção
 void gpio_irq_handler(uint gpio, uint32_t events) {
-    // Armazena o tempo atual em microssegundos
     uint32_t current_time = to_us_since_boot(get_absolute_time());
-    // Verifica se passou tempo suficiente desde o último evento (debouncing)
-    if (current_time - last_time > 200000) { // 200ms de debounce
-        last_time = current_time; // Atualiza o tempo do último evento
-        if (gpio == BUTTON_A_PIN) { // Botão A pressionado
-            leds_ativos = !leds_ativos; // Inverte o estado dos LEDs
-
-            if (!leds_ativos) { // Se os LEDs foram desativados, apaga todos
-                gpio_put(LED_AZUL_PIN, 0); // Desliga o LED azul
-                gpio_put(LED_VERMELHO_PIN, 0); // Desliga o LED vermelho
-                gpio_put(LED_VERDE_PIN, 0); // Desliga o LED verde
-            } else {
-                gpio_put(LED_AZUL_PIN, 1); // Liga o LED azul
-                gpio_put(LED_VERMELHO_PIN, 1); // Liga o LED vermelho
-            }
+    if (current_time - last_time > 200000) {
+        last_time = current_time;
+        if (gpio == BUTTON_A_PIN) {
+           
         }
-
-        if (gpio == JOYSTICK_BUTTON_PIN) { // Botão do joystick pressionado
-            borda_display = !borda_display; // Alterna a borda do display
-            gpio_put(LED_VERDE_PIN, !gpio_get(LED_VERDE_PIN)); // Alterna o estado do LED verde
+        if (gpio == JOYSTICK_BUTTON_PIN) {
+            borda_display = !borda_display;
+            gpio_put(LED_VERDE_PIN, !gpio_get(LED_VERDE_PIN));
         }
     }
+}
+
+void init_pwm(uint gpio) {
+    gpio_set_function(gpio, GPIO_FUNC_PWM);
+    uint slice = pwm_gpio_to_slice_num(gpio);
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_wrap(&config, 65535);
+    pwm_init(slice, &config, true);
+    pwm_set_gpio_level(gpio, 0);
+}
+
+void set_led_brightness(uint gpio, uint16_t value) {
+    pwm_set_gpio_level(gpio, value);
 }
